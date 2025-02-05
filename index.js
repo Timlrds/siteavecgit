66fddbec338ace47ba4a737d88ade19f441cde36
Timao Gourdais <timaogourdais@MacBook-Pro-de-Timao.local>
2025-02-05 16:20:44 +0100

    loup

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 
 
-console.log("njhnnjop")
+
+
+const baliseTitreH4 = document.querySelector("h4")
+baliseTitreH4.style.background = "blue"
+
+const questionContainer = document.querySelector(".onclick");
+console.log(questionContainer);
